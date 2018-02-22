e60fa885cf35adbfd3e9faf9d5b05cbd1e01e795
Rian <rian.sj@gmail.com>
2018-02-22 22:13:24 +0700

    completing linked list

--- LinkedLists/LinkedList.js
+++ LinkedLists/LinkedList.js
@@ -7,15 +7,15 @@ function LinkedList() {
 	let length = 0;
 	let head = null;
 
-	// {1} crate node item passing element as its value
-	// {2} a variable that points to the current list
-	// {3} if head is null (the list is empty), we are the first element to the list
-	// 		if head is not null
-	// 		{4} reference to the first element, we iterate the list to find the last element (current.next is null)
-	//		{5} then, link the current next element  to point to the node we want to add
-	// {6} increment the size of the list
-
 	this.append = function(element) {
+		// {1} crate node item passing element as its value
+		// {2} a variable that points to the current list
+		// {3} if head is null (the list is empty), we are the first element to the list
+		// 		if head is not null
+		// 		{4} reference to the first element, we iterate the list to find the last element (current.next is null)
+		//		{5} then, link the current next element  to point to the node we want to add
+		// {6} increment the size of the list
+
 		let node = new Node(element),	// {1}
 			current;					// {2}
 
@@ -32,16 +32,91 @@ function LinkedList() {
 			current.next = node;			// {5} get last item and assign next to  node
 		}
 
-		length++						// {6} update the size of list
+		length++;						// {6} update the size of list
 	};
 
-	this.insert = function(position, element) {};
-	this.removeAt = function(position) {};
-	this.remove = function(element) {};
-	this.indexOf = function(element) {};
+	this.insert = function(position, element) {
+		// {1} we need to check the out-of-bound position
+		// {6} if it is out of bound, we return the value false to indicate that no item was added to the list
+		// First scenario is the case where we need to add an element at the beginning of the list, fist position
+
+		// check for out-of-bounds values
+		if (position >= 0 && position <= length) {	// {1}
+			let node = new Node(element),
+				previous, current = head,
+				index = 0;
+
+			if (position === 0) {					// add on first position
+				node.next = current;				// {2}
+				head = node;
+
+			} else {
+				while (index++ < position) {		// {3}
+					previous = current;	
+					current = current.next;
+				}
+				node.next = current;				// {4}
+				previous.next = node;				// {5}
+			}
+
+			length++;								// update size of list
+			return true;
+
+		} else {
+			return false;							// {6}
+		}
+
+	};
+
+	this.removeAt = function(position) {
+		// check for out-of-bounds values
+		if (position > -1 && position < length) {	// {1} verify the position value is valid
+			let current = head,						// {2} variable reference to the current node
+				previous,							// {3} variable reference to the previous node
+				index = 0;							// {4}
+
+			if (position === 0) {					// {5} removing first item
+				head = current.next
+			} else {
+				while (index++ < position) {		// {6} iterate the list until the desired position
+					previous = current;				// {7}
+					current = current.next			// {8}
+				}
+				previous.next = current.next;		// {9}
+			}
+
+			length--;								// {10}
+
+			return current.element;
+
+		} else {
+			return null;							// {11}
+		}
+	};
+
+
+	this.indexOf = function(element) {
+		let current = head,
+			index = -1;
+
+		while (current) {
+			index++;
+			if (current.element === element) {
+				return index;
+			}
+			current = current.next;
+		}
+
+		return -1;
+	};
+
+	this.remove = function(element) {
+		let index = this.indexOf(element);
+		this.removeAt(index);
+	};
 
 	this.isEmpty = function() {
-		return (head === null);
+		return length === 0;
 	};
 
 	this.size = function() {
@@ -49,20 +124,20 @@ function LinkedList() {
 	};
 
 	this.toString = function() {
-		let elements = [],
-			current;
+		let current = head,
+			string = "";
 
-		if (head !== null) {
-			current = head;
-			elements.push(current.element);
-			while (current.next !== null) {
-				current = current.next;
-				elements.push(current.element);
-			}
+		while (current) {
+			string += current.element + (current.next ? ', ' : '');
+			current = current.next;
 		}
 
-		return elements.toString();
+		return string;
 	};
+
+	this.getHead = function() {
+		return head;
+	}
 
 	this.print = function() {};
 }
