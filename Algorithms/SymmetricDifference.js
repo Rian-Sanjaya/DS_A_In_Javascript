643dee236ca1b76366b4f51213de33b78c430f2c
Rian Sanjaya <rian.sj@gmail.com>
2019-01-15 17:31:15 +0700

    update symmetricdifference

--- Algorithms/SymmetricDifference.js
+++ Algorithms/SymmetricDifference.js
@@ -1,3 +1,11 @@
+// Given two sets (for example set A = {1, 2, 3} and set B = {2, 3, 4}), 
+// the mathematical term "symmetric difference" of two sets is the set of elements which are in either of the two sets, 
+// but not in both (A △ B = C = {1, 4}). 
+// For every additional symmetric difference you take (say on a set D = {2, 3}), 
+// you should get the set with elements which are in either of the two the sets but not both 
+// (C △ D = {1, 4} △ {2, 3} = {1, 2, 3, 4}). 
+// The resulting array must contain only unique values (no duplicates).
+
 function sym() {
     let args = [];
 
@@ -5,9 +13,9 @@ function sym() {
         args.push(arguments[i]);
     }
 
-    return args.reduce( (arrOne, arrTwo) => {
+    const symDiff = (arrOne, arrTwo) => {
         let results = [];
-        
+
         arrOne.forEach( el => {
             if (arrTwo.indexOf(el) < 0 && results.indexOf(el) < 0) {
                 results.push(el);
@@ -21,6 +29,10 @@ function sym() {
         });
 
         return results;
-    });
+    };
+
+    return args.reduce( symDiff )
 }
 
+sym([1, 2, 3, 3], [5, 2, 1, 4]); // should return [3, 4, 5]
+sym([1, 1, 2, 5], [2, 2, 3, 5], [3, 4, 5, 5]); // should return [1, 4, 5]
