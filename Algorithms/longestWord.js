fd9d54f4c0ed6245c1f419b9a83856aa99c687b8
Rian Sanjaya <rian.sj@gmail.com>
2024-07-11 20:43:53 +0700

    clean code longestWord

--- Algorithms/longestWord.js
+++ Algorithms/longestWord.js
@@ -1,8 +1,26 @@
-// Input: "This is a demo String find the largest word from it"
-// Output: "largest"
+// Input: "This is a demo String find the longest word from it"
+// Output: "longest"
 
 // Input:  "Hello guys this is geeksforgeeks where students learn programming"
 // Output: "geeksforgeeks"
+
+const longestWord = (string) => {
+  const str = string.split(' ')
+  let longest = ''
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i].length > longest.length) {
+      longest = str[i]
+    }
+  }
+
+  return longest
+}
+
+console.log(longestWord(
+  "Hello guys this is geeksforgeeks where students learn programming"
+))
+  
 
 function longestWord(str){ 
   // Split the string using regex 
@@ -15,31 +33,12 @@ function longestWord(str){
       // If the i'th item is greater than largest string 
       // then overwrite the largest string with the i'th value 
       if(str[i].length > largest.length){ 
-      largest = str[i] 
+       largest = str[i] 
       } 
   } 
+
   return largest; 
-  } 
+} 
    
-  console.log(longestWord("Hello guys this is geeksforgeeks where"+ 
-                      " students learn programming")) 
-
-
-  let longestWord = (string) => {
-    let stringg = string.split(" ");
-    let longest = 0;
-    let longest_word = null;
-    for (let i = 0; i < stringg.length; i++) {
-      if (longest < stringg[i].length) {
-        longest = stringg[i].length;
-        longest_word = stringg[i];
-      }
-    }
-    return longest_word;
-  };
-    
-  console.log(
-    longestWord(
-  "Hello guys this is geeksforgeeks where students learn programming"
-    )
-  );
+console.log(longestWord("Hello guys this is geeksforgeeks where"+ 
+                    " students learn programming")) 
